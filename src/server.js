9fd91dca68b9a22756697a951e841e902ccda202
David Rueda <davd33@gmail.com>
2018-05-30 15:45:13 +0545

    change to port 80

--- src/server.js
+++ src/server.js
@@ -3,4 +3,4 @@ const app = express()
 
 app.use(express.static('../public'))
 
-app.listen(3000, () => console.log('Listening on port 3000'))
+app.listen(80, () => console.log('Listening on port 3000'))
