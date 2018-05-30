340681d7c0781bdb3b70a4bdcc5219ca5724f634
David Rueda <davd33@gmail.com>
2018-05-30 16:13:09 +0545

    cannot get public

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('../public'))
+app.use(express.static('public'))
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.log('Listening on port ' + PORT))
